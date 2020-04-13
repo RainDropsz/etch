64251062631f34cd5ceff18c5850fbee3850cda7
RainDropsz <shinida@gmail.com>
2020-04-13 19:50:12 -0400

    fix colors

--- etch1.js
+++ etch1.js
@@ -1,25 +1,23 @@
          //Global Variables
-let mode = 0;
-let color1 = $("#colorChoice").val();
-let colorText = $("#colorText").val();
+var mode = 0;
+var color1 = $("#colorChoice").val();
+var colorText = $("#colorText").val();
 
-
-// Functions
 function buildGrid(nwide, nhght) {
-  let i = 0, j = 0;
-  let mode = 0;
+  var i = 0, j = 0;
+  var mode = 0;
 
           //Remove previous grid
   $("#grid1").remove();
-          //Add div #grid1 after #forms
+          //Add div #grid1 as grid container
   $("#forms").after('<div id="grid1"></div>');
 
-  for(i=1; i <= nhght; i++) {
+  for(i=1;i<=nhght;i++) {
           //div .gridrow is row container
     $("#grid1").append('<div class="gridrow"></div>');
 
           //div .boxTp1 is each box in the row
-    for( j = 1; j <= nwide; j++) {
+    for(j=1;j<=nwide;j++) {
       $(".gridrow:last-child").
       append("<div class='boxTp1'></div>");
     }
@@ -27,33 +25,10 @@ function buildGrid(nwide, nhght) {
          //css to fit boxes to screen
   addboxcss(nwide, nhght);
          //attach event listener to new grid
-  $(".boxTp1").mouseover((e) => colorMode(e));
+  $(".boxTp1").mouseover(() => colorMode(event))  ;
   gradinit(); // initialize gradient
 }        //end of function buildGrd
 
-
-function colorMode(e) {
-         //Modes for changing box colors
-  var str0 = "", opcStart = 0, curropc = "", newopc = 0, rgba = "";
-
-  if (mode == 0) {
-    $(e.target).css("background-color", color1);}
-  else if (mode == 1) {
-    applyRandColor();
-    $(e.target).css("background-color", color1);}
-  else if (mode == 2) {
-    let colorstr = $(e.target).css("background-color");
-    let startind = (colorstr.lastIndexOf(",") + 1);
-    let curropc = parseFloat(colorstr.slice(startind));
-    let newopc = curropc + 0.1;
-    let newcolor = colorstr.slice(0, startind) + newopc + ")";
-
-    $(e.target).css("background-color", newcolor);
-  }
-}
-
-
-// Helper Functions
 function addboxcss(nwide, nhght) {
          //calculate good percentages for screen size
   var pxwide = (.9*window.innerWidth / nwide -2) ;
@@ -64,6 +39,28 @@ function addboxcss(nwide, nhght) {
 
   $(".boxTp1").css("width",  pctwide);
   $(".boxTp1").css("height", pcthght);
+}
+
+function colorMode(e) {
+         //Modes for changing box colors
+  var str0 = "", opcStart = 0, curropc = "", newopc = 0, rgba = "";
+  const opacity = 0.2; 
+
+
+  if (mode == 0) {
+    $(e.target).css("background-color", color1);}
+  else if (mode == 1) {
+    applyRandColor();
+    $(e.target).css("background-color", color1);}
+  else if (mode == 2) {
+    str0 = $(e.target).css("background-color");
+    opcStart = (str0.lastIndexOf(",") + 1);
+    curropc = parseFloat(str0.slice(opcStart));
+    newopc = curropc + 0.2;
+    rgba = str0.slice(0, opcStart) + newopc + ")";
+
+    $(e.target).css("background-color", rgba);
+  }
 }
 
          //Event Listener: Make new grid
@@ -82,10 +79,10 @@ $("#colorChoice").on("change",
   }
 );
 
-        //Event Listener for enter key on colorText changes
+          //Enter key on colorText changes
 $(window).keydown(
   function(event){                   //if press enter after
-    if(event.key == "Enter" &&        //typing in colorText
+    if(event.keyCode == 13 &&        //typing in colorText
       $("#colorText").is(":focus")) {
         event.preventDefault();
 
@@ -127,23 +124,22 @@ function gradinit() {
   $(".boxTp1").each(
     function() {
       str1 = $(this).css("background-color");
-      $(this).css("background-color", convToRGBA(str1) );
+      $(this).css("background", convToRBGA(str1) );
     }
   );
 }
 
-function convToRGBA(tmprgb)  {
+function convToRBGA(tmprgb)  { 
 
   if(tmprgb.search("rgba") < 0) {
     tmprgb = tmprgb.slice(0,3) + "a" +
-      tmprgb.slice(3,tmprgb.length-1) + ", " + 0 + ")";
+      tmprgb.slice(3,tmprgb.length-1) + "," + 1 + ")";
   }
   else {
-    tmprgb="rgba(255,255,255,0)";
+    tmprgb="rgba(0,0,0,1)";
   }
 
-  // return "rgba(255,255,255,0)";
-  return tmprgb;
+//  return "rgba(255,255,255,0)";
 }
 
          //prevent reload page on form submit
